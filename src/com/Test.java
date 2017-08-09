1d3c8fbff52d6873a10f9bd1d5b296b623b4787c
turetchimihai <turetchi.mihai@gmail.com>
2017-08-09 14:23:25 +0300

    Add line 6 patch

--- src/com/Test.java
+++ src/com/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("nub");
+        System.out.println("patch");
+        System.out.println("test branch");
     }
 }
